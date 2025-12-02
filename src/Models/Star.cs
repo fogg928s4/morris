86f30d67ac61987c107728e156c93282f2cfda84
J Melg <rjmzelaya@gmail.com>
2025-12-02 22:19:34 +0100

    feat: creates classes for celestial bodies

---
+++ src/Models/Star.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morris.Models {
+    internal class Star : CelestialBody {
+
+        // Constructors
+        public Star() {
+            this.name = "Sun";
+            this.baseMass = 1.998;
+            this.massOrderPower = 30;
+            this.trueMass = baseMass * Math.Pow(10, massOrderPower);
+        }
+        public Star(string name, double baseMass) {
+            this.name = name;
+            this.baseMass = baseMass;
+            this.massOrderPower = 30;
+            this.trueMass = this.baseMass * Math.Pow(10, massOrderPower);
+        }
+        public Star(string name) {
+            this.name = name;
+            this.baseMass = 5.97;
+            this.massOrderPower = 24;
+            this.trueMass = baseMass * Math.Pow(10, massOrderPower);
+        }
+
+
+        public int MassOrderPower {
+            set { massOrderPower = 30; }
+        }
+    }
+}
