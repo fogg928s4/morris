bb7ef17593b68f01db6708f582d5dd3d89c35cc3
J Melg <rjmzelaya@gmail.com>
2025-12-05 01:01:17 +0100

    FEAT: fetch data from JSON in user documents dir

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -5,19 +5,20 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using Morris.Models;
 using System;
 //using System.Drawing;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.UI.Core;
-using Microsoft.UI.Xaml.Shapes;
-using Morris.Models;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Morris
 {
@@ -26,12 +27,34 @@ namespace Morris
     {
         //initializes main window
         public MainWindow() {
-            this.InitializeComponent();
-            Title = "Morris";
+            try {
+                this.InitializeComponent();
+                Title = "Morris";
+                FetchJSONSimData("\\StarSystem.json");
+            }
+            catch (Exception ex) {
+                if(ex is FileNotFoundException) {
+                    Console.WriteLine(ex.Message);
+                    throw new FileNotFoundException(Title);
+                }
+                this.Close();
+            }
         }
 
-        private StarSystem currentSimulation = new StarSystem();
+        public StarSystem currentSimulation;
 
+        // fetch data from JSON file
+        public async void FetchJSONSimData(string fileName) {
+            string fileDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\SimData" ;
+            string fullPath = fileDirectory + fileName;
 
+            if (!File.Exists(fullPath)) {
+                Directory.CreateDirectory(fileDirectory);
+                File.Create(fullPath);
+
+            }
+            await using Stream openStream = File.OpenRead(fullPath);
+            currentSimulation = await JsonSerializer.DeserializeAsync<StarSystem>(openStream);
+        }
     }
 }
--- src/Models/StarSystem.cs
+++ src/Models/StarSystem.cs
@@ -6,9 +6,11 @@ using System;
 namespace Morris.Models {
 
     public class StarSystem {
+        
         private IList<Planet> planets;
         private Star mainStar;
         private int planetCount;
+        public string Title { get; set; }
 
         public StarSystem() {
             this.mainStar = new Star();
@@ -32,16 +34,12 @@ namespace Morris.Models {
 
 
         private async void WriteSimToJSON() {
-            string fileName = "Data/StarSystem.json";
+            string fileName = "StarSystem.json";
             await using FileStream createStream = File.Create(fileName);
             var options = new JsonSerializerOptions { WriteIndented = true };
             await JsonSerializer.SerializeAsync(createStream, this, options);
         }
 
-        private async void ReadSimFromJSON() {
-            string fileName = "Data/StarSystem.json";
-            using FileStream openStream = File.OpenRead(fileName);
-            StarSystem readInfo = await JsonSerializer.DeserializeAsync<StarSystem>(openStream);
-        }
+
     }
 }
